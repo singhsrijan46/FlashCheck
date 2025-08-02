9fa76ddaeb9e75dfab426a7b32ab07e9cd303173
singhsrijan46 <singhsrijangkp@gmail.com>
2025-08-02 23:00:11 +0530

    small fix

--- server/routes/showRoutes.js
+++ server/routes/showRoutes.js
@@ -1,12 +1,20 @@
 import express from "express";
-import { addShow, getNowPlayingMovies, getShow, getShows, getMovieTrailer } from "../controllers/showController.js";
+import { addShow, getNowPlayingMovies, getShow, getShows, getMovieTrailer, debugMovies, updateMovieWithCompleteData, testTMDBAPI, checkEnvironment, searchMovies, getPopularMovies, debugShows } from "../controllers/showController.js";
 import { protectAdmin } from "../middleware/auth.js";
 
 const showRouter = express.Router();
 
 // Specific routes first
 showRouter.get('/now-playing', protectAdmin, getNowPlayingMovies)
+showRouter.get('/now-playing-public', getNowPlayingMovies) // Public route for testing
 showRouter.get("/all", getShows)
+showRouter.get("/debug", debugMovies)
+showRouter.get("/debug-shows", debugShows)
+showRouter.get("/test-tmdb", testTMDBAPI)
+showRouter.get("/check-env", checkEnvironment)
+showRouter.get("/search", searchMovies)
+showRouter.get("/popular", getPopularMovies)
+showRouter.put("/update-movie/:movieId", protectAdmin, updateMovieWithCompleteData)
 
 // Parameterized routes last
 showRouter.get("/:movieId", getShow)
